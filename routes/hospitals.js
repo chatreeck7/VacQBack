3ccd71a67c2f2c7fa0b6d956562df898945d142c
chatreeck7 <chatree.devl@gmail.com>
2023-02-01 22:58:29 +0700

    Complete controller to manage logic of API after it is routed !

--- routes/hospitals.js
+++ routes/hospitals.js
@@ -1,30 +1,8 @@
 const express = require('express');
-const dotenv = require('dotenv');
-
-//Load env vars
-dotenv.config({path:'./config/config.env'})
-
 const router = express.Router();
+const { getHospitals, getHospital, createHospital, updateHospital, deleteHospital } = require('../controllers/hospitals')
 
-//GET ALL HOSPITALS
-router.get('/', (req,res) => {
-    res.status(200).json({success:true, msg:"Show all hospitals"});
-});
-//GET ONE HOSPITAL BY ID
-router.get('/:id', (req,res) => {
-    res.status(200).json({success:true, msg:`Show hospital: ${req.params.id}`});
-});
-//POST HOSTITALS
-router.post('/', (req,res) => {
-    res.status(200).json({success:true, msg:"Create new hospital"});
-});
-//PUT ONE HOSPITAL
-router.put('/:id', (req,res) => {
-    res.status(200).json({success:true, msg:`Update hospital: ${req.params.id}`});
-});
-//DELETE ONE HOSPITAL
-router.delete('/:id', (req,res) => {
-    res.status(200).json({success:true, msg:`Delete hospital: ${req.params.id}`});
-});
+router.route('/').get(getHospitals).post(createHospital);
+router.route('/:id').get(getHospital).put(updateHospital).delete(deleteHospital);
 
 module.exports = router;
