e4a7049bf0d06c348de9923e3da8160a8d3d102b
chatreeck7 <chatree.devl@gmail.com>
2023-02-01 22:33:35 +0700

    Completely the beginning of hospitals API!

--- server.js
+++ server.js
@@ -7,14 +7,29 @@ dotenv.config({path:'./config/config.env'})
 const app = express();
 
 app.get('/', (req,res) => {
-    // function that i may want to use it instruction
-    // res.send('<h1>Hello from express</h1>');
-    // res.send({name:'Brad'});
-    // res.json({name:'Brad'});
-    // res.sendStatus(400);
-    // res.status(400).json({success:false});
     res.status(200).json({success:true, data:{id:1}})
-})
+});
+
+//GET ALL HOSPITALS
+app.get('/api/v1/hospitals', (req,res) => {
+    res.status(200).json({success:true, msg:"Show all hospitals"});
+});
+//GET ONE HOSPITAL BY ID
+app.get('/api/v1/hospitals/:id', (req,res) => {
+    res.status(200).json({success:true, msg:`Show hospital: ${req.params.id}`});
+});
+//POST HOSTITALS
+app.post('/api/v1/hospitals', (req,res) => {
+    res.status(200).json({success:true, msg:"Create new hospital"});
+});
+//PUT ONE HOSPITAL
+app.put('/api/v1/hospitals/:id', (req,res) => {
+    res.status(200).json({success:true, msg:`Update hospital: ${req.params.id}`});
+});
+//DELETE ONE HOSPITAL
+app.delete('/api/v1/hospitals/:id', (req,res) => {
+    res.status(200).json({success:true, msg:`Delete hospital: ${req.params.id}`});
+});
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV,' mode on_port', PORT))
