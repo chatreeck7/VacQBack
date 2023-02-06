9e35106f6bf97db6643914fedccd11e953b68a57
Chatree Kurupakorn <chatree.devl@gmail.com>
2023-02-06 14:45:43 +0700

    Setting the schema and mongoDB connection

---
+++ config/db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+// async function is used to connect to the database 
+// by await mongoose.connect(process.env.MONGO_URI)
+const connectDB = async () => {
+    mongoose.set('strictQuery', true);
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`); 
+    // conn.connection.host is the host name of the database
+}
+
+module.exports = connectDB;
