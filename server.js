1fb18359e092424cfd5dfb209052f1858741161b
chatreeck7 <chatree.devl@gmail.com>
2023-04-04 22:13:17 +0700

    ➕ Add code to prevent SQL injection

--- server.js
+++ server.js
@@ -1,41 +1,48 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const connectDB = require('./config/db');
-const cookieParser = require('cookie-parser');
-var cors = require('cors')
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const cookieParser = require("cookie-parser");
+const mongoSanitize = require("express-mongo-sanitize");
+var cors = require("cors");
 
 //Load env vars
-dotenv.config({path:'./config/config.env'})
+dotenv.config({ path: "./config/config.env" });
 
 //Connect to database
 connectDB();
 
 const app = express();
 
-app.use(cors())
+//CORS
+app.use(cors());
 //Body parser
 app.use(express.json());
+//Sanitize data
+app.use(mongoSanitize());
 //Mount routers
-const hospitals = require('./routes/hospitals');
-const appointments = require('./routes/appointments');
-const auth = require('./routes/auth');
+const hospitals = require("./routes/hospitals");
+const appointments = require("./routes/appointments");
+const auth = require("./routes/auth");
 //Cookie parser
 app.use(cookieParser());
 
-app.get('/', (req,res) => {
-    res.status(200).json({success:true, data:{id:1}})
+app.get("/", (req, res) => {
+  res.status(200).json({ success: true, data: { id: 1 } });
 });
 
-app.use('/api/v1/hospitals', hospitals);
-app.use('/api/v1/auth', auth);
-app.use('/api/v1/appointments', appointments);
+app.use("/api/v1/hospitals", hospitals);
+app.use("/api/v1/auth", auth);
+app.use("/api/v1/appointments", appointments);
 
 const PORT = process.env.PORT || 5001;
-const server = app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV,' mode on_port', PORT));
+const server = app.listen(
+  PORT,
+  console.log("Server running in ", process.env.NODE_ENV, " mode on_port", PORT)
+);
 
 //Handle unhandled promise rejections
-process.on('unhandledRejection', (err, promise) => {
-    console.log(`Error: ${err.message}`);
-    //Close server & exit process
-    server.close(() => process.exit(1));
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  //Close server & exit process
+  server.close(() => process.exit(1));
 });
